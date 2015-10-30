ccb6f3e6f47cb4adde2bf7cbada1b0745d779422
reki andika <reki.andika@gmail.com>
2015-10-30 16:23:02 +0700

    Update 2

---
+++ 01-Vector/Vector3D.java
@@ -0,0 +1,24 @@
+
+/**
+ * Write a description of class Vector3D here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Vector3D
+{
+    // instance variables - replace the example below with your own
+    public double x;
+    public double y;
+    public double z;
+
+    /**
+     * Constructor for objects of class Vector3D
+     */
+    public Vector3D(double x, double y, double z)
+    {
+        this.x=x;
+        this.y=y;
+        this.z=z;
+    }
+}
