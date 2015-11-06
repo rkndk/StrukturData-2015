91c97a7d4869302c3485907833f077688122a942
reki andika <reki.andika@gmail.com>
2015-11-06 14:45:12 +0700

    Preview Test Awal : Profil

---
+++ 04-Profil-Test-Awal/Profil.java
@@ -0,0 +1,25 @@
+public class Profil
+{
+    private String nama = "M REKI ANDIKA";
+    private String nim = "1408107010001";
+
+    public Profil()
+    {
+    }
+
+    public Profil(String nama, String nim)
+    {
+        this.nama=nama;
+        this.nim=nim;
+    }
+    
+    public String getNama()
+    {
+        return nama;
+    }
+    
+    public String getNim()
+    {
+        return nim;
+    }
+}
