ebf2f33e68acf66732b83ed426d76c4ab9bd4af0
reki andika <reki.andika@gmail.com>
2015-11-12 14:51:15 +0700

    Tugas 2 Komdat

--- 09-Upper-Tugas/Upper.java
+++ 09-Upper-Tugas/Upper.java
@@ -1,5 +1,6 @@
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
+/* Sesuai soal, asumsi sudah pasti file adalah berkas text, maka menggunakan FileReader dan FileWriter */
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class Upper{
@@ -14,11 +15,11 @@ public class Upper{
     }
 
     public void upper(String sumber, String sasaran) throws IOException {
-        FileInputStream masukan = null;
-        FileOutputStream keluaran = null;
+        FileReader masukan = null;
+        FileWriter keluaran = null;
         try {
-            masukan = new FileInputStream(sumber);
-            keluaran = new FileOutputStream(sasaran);
+            masukan = new FileReader(sumber);
+            keluaran = new FileWriter(sasaran);
             int karakter = masukan.read();
             while (karakter != -1) {
                 karakter = Character.toUpperCase(karakter);
