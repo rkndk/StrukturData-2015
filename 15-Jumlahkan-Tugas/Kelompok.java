8d084a338e662180470a96899f32ffc2f2565c72
reki andika <reki.andika@gmail.com>
2015-11-20 18:16:34 +0700

    Praktikum

---
+++ 15-Jumlahkan-Tugas/Kelompok.java
@@ -0,0 +1,29 @@
+public class Kelompok implements Runnable
+{
+    private int awal;
+    private int akhir;
+    private double[] data;
+    private double nilaiHasil=0;
+
+    public Kelompok(int awal, int akhir, double[] data)
+    {
+        this.awal=awal;
+        this.akhir=akhir;
+        this.data=data;
+    }
+
+    public double hasil()
+    {
+        return nilaiHasil;
+    }
+
+    public void hitung()
+    {
+        for( int counter = awal ; counter<=akhir;counter++)
+            nilaiHasil+=data[counter];
+    }
+
+    public void run() {
+        hitung();
+    }
+}
